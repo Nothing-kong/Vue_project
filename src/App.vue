3000e0b5ad428f73470fbcaa6aab421a96611cb4
KLong <15110232612@163.com>
2019-12-12 14:38:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>尚硅谷</h1>
+    <button class='btn btn1'>按钮1</button>
+    <button class='btn btn2'>按钮2</button>
+    <ul>
+      <li v-for="(item, index) in 20" :key="index"
+      @click='choose(index)' :class='{active:navIndex === index}'
+      >{{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      navIndex:0
+    }
+  },
+  methods: {
+    choose(index){
+      this.navIndex = index
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='stylus' rel='stylesheet/stylus'>
+#app
+  $color = green
+  btnStyle(w, h, bg)
+    width w
+    height h
+    background bg
+  h1
+    color orange 
+    background-color #aaaaaa
+    font-size 30px
+    display inline-block
+    position absolute
+    margin-left 50%
+    transform translateX(-50%)
+  .btn1
+    btnStyle(100px, 100px, red)
+  .btn2
+    btnStyle(100px, 100px, blue)
+    float right
+  ul
+    li
+      line-height 50px
+      &.active
+        color red
 </style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  runtimeCompiler: true,
+  // 关闭ESlint, 默认为true，编译后在控制台警告错误信息，但是不影响编译，
+  // 设置为‘error’的时候在页面报错显示错误内容，设置为false关闭提示
+  lintOnSave: false,
+}
